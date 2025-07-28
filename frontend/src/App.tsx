07f70a7ccbb1db5eb53b6651901884751f50dc9d
c0l0b0k <212red212@mail.ru>
2025-07-28 21:40:12 +0500

    chore: remove unused TSX pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,4 @@
 import React from "react";
-import { HomePage } from "./pages/HomePage";
-import ResponsivePlotCard from "./components/ResponsivePlotCard";
-import FileUpload from "./components/FileUpload";
-import UploadFile from "./components/UploadFile";
-import {Tabs} from "./components/Tabs";
 import Dashboard from "./components/Dashboard";
 
 function App() {
